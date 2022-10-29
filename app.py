b4a1d5ecac857843db630b5e06297f08a487a101
Binh An <anbinhpham110799@gmail.com>
2022-10-29 11:01:13 +0700

    removing redundant env variables

--- app.py
+++ app.py
@@ -9,13 +9,13 @@ from selenium.webdriver.support.ui import WebDriverWait, Select
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
 from datetime import datetime
 
 load_dotenv()
 MANAGER_USERNAME = os.getenv("MANAGER_USERNAME")
 MANAGER_PASSWORD = os.getenv("MANAGER_PASSWORD")
-CHROMEDRIVER_PATH = os.environ.get("CHROMEDRIVER_PATH")
-GOOGLE_CHROME_BIN = os.environ.get("GOOGLE_CHROME_BIN")
 
 class HelloFrame(wx.Frame):
     """
@@ -73,11 +73,10 @@ class HelloFrame(wx.Frame):
         # initialize the Chrome driver
         options = Options()
         options.add_experimental_option('excludeSwitches', ['enable-logging'])
-        options.binary_location = GOOGLE_CHROME_BIN
         options.add_argument('--headless')
         options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--no-sandbox")
-        driver = webdriver.Chrome(chrome_options=options, executable_path=CHROMEDRIVER_PATH)
+        driver = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
         # login page
         driver.get("https://trivietedu.ileader.vn/login.aspx")
         # find username/email field and send the username itself to the input field
@@ -183,6 +182,7 @@ class HelloFrame(wx.Frame):
             output_df.to_excel(writer, sheet_name='Sheet1')
             # Close the Pandas Excel writer and output the Excel file to the buffer
             writer.save()
+        driver.quit()
         '''
         print(f"Total No of Students: {output_df['Sĩ số'].sum()}")
         end_time = datetime.now()
